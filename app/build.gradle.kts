e15425eb2bb6b77be892d9e44a26494844674112
Michał Dziwisz <michal@dziwisz.net>
2026-07-12 01:27:21 +0200

    chore: bump versionCode to 6 for store release (Roman feedback: Mow co 1s, Tolerancja zadanego kursu 1-5, removed tone toggles + reference tone)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "eu.blueseaeye.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.0"
 
         vectorDrawables {
